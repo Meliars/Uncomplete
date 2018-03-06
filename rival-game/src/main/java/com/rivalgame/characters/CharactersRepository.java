0ad3048f1eb548083e2dabdd0ffb5c29e4367c4f
Paweł Jaworski <pawel.jaworski94@o2.pl>
2018-03-06 17:37:08 +0100

    1 stage - complete.

--- rival-game/src/main/java/com/rivalgame/characters/CharactersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rivalgame.characters;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-
-public interface CharactersRepository extends CrudRepository <Characters, Long> {
-}
