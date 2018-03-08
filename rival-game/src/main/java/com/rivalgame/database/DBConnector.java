972d34e3fd5d3fa3ef9e0a68e59019b1c8ac4751
Paweł Jaworski <pawel.jaworski94@o2.pl>
2018-03-08 17:43:05 +0100

    Using Hibernate

--- rival-game/src/main/java/com/rivalgame/database/DBConnector.java
+++ rival-game/src/main/java/com/rivalgame/database/DBConnector.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import java.util.List;
 
 @Component
 public class DBConnector implements DataB {
@@ -18,7 +16,7 @@ public class DBConnector implements DataB {
 
     @Override
     public void setUp() throws Exception {
-       EntityManagerFactory sessionFactory= Persistence.createEntityManagerFactory("transcom");
+       EntityManagerFactory sessionFactory= Persistence.createEntityManagerFactory("transcom-db");
 
     }
 
