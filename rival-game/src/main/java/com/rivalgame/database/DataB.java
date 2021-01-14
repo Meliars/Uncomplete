98f352b97df5227d3cd446b2202741642f5a9929
Meliars <kimonki4@gmail.com>
2021-01-14 18:36:23 +0100

    Delete rival-game directory

--- rival-game/src/main/java/com/rivalgame/database/DataB.java
+++
@@ -1,9 +0,0 @@
-package com.rivalgame.database;
-
-import com.rivalgame.characters.Characters;
-
-public interface DataB {
-    public void setUp() throws Exception;
-    public void save(Characters characters);
-    public void read();
-}
